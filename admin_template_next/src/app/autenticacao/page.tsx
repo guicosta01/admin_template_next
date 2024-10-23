c8edf2f4fca5bb8de81216a78c0f17838598c831
sesa697353 <guilhermeferreira.costa@se.com>
2024-10-23 12:09:13 -0300

    iniciando tela de login

---
+++ admin_template_next/src/app/autenticacao/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useState } from 'react';
+import AuthInput from "@/components/auth/AuthInput";
+
+
+export default function Autenticacao() {
+
+    const [modo, setModo] = useState<'login' | 'cadastro'>('login');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+
+    function submeter(){
+        if(modo === 'login'){
+            console.log('login');
+        } else {
+            console.log('cadastro')
+        }
+    }
+
+
+    return (
+        <div className='flex flex-col h-screen items-center justify-center'>
+            <div className='w-1/2'>
+                <h1 className='text-xl font-bold mb-5'>
+                    {modo === 'login' ? 'Entre com sua conta' : 'Cadastre-se no site'}
+                </h1>
+                <AuthInput
+                    label="Email"
+                    tipo='email'
+                    valor={email}
+                    valorMudou={setEmail}
+                    obrigatorio
+                />
+                <AuthInput
+                    label="Senha"
+                    tipo='password'
+                    valor={senha}
+                    valorMudou={setSenha}
+                    obrigatorio
+                />
+
+                <button onClick={submeter} className='
+                    w-full bg-indigo-500 hover:bg-indigo-400
+                    text-white rounded-lg px-4 py-2 mt-6
+                '>
+                    {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+
+
+                <hr className='my-6 border-gray-300 w-full'/>
+
+                <button onClick={submeter} className='
+                    w-full bg-red-500 hover:bg-red-400
+                    text-white rounded-lg px-4 py-2
+                '>
+                    Entrar com o Google
+                </button>
+
+                
+            </div>
+        </div>
+    );
+}
--- admin_template_next/src/components/template/Logo.tsx
+++ admin_template_next/src/components/template/Logo.tsx
@@ -1,10 +1,9 @@
 export default function Logo() {
     return (
-        <div className="
-            flex flex-col items-center justify-center
-            bg-white
-            h-10 w-10 rounded-full">
-            <div className="h-3 w-3 rounded-full bg-red-600"></div>
-        </div>
-    )
+        <img 
+            src="../../assets/urso_logo.png" 
+            alt="Logo" 
+            className="h-10 w-10 rounded-full"
+        />
+    );
 }
